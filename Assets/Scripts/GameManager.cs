d6400f9719fb14022b565954f15a5b4bf4aed97a
bioroid17 <trace1997@ajou.ac.kr>
2025-01-12 03:28:52 +0900

    High Scores

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -14,15 +15,20 @@ public class GameManager : MonoBehaviour
 
     public GameState State = GameState.Intro;
 
+    public float PlayStartTime;
+
     public int Lives = 3;
 
     [Header("References")]
     public GameObject IntroUI;
+    public GameObject DeadUI;
     public GameObject EnemySpawner;
     public GameObject FoodSpawner;
     public GameObject GoldSpawner;
 
     public Player PlayerScript;
+
+    public TMP_Text ScoreText;
 
     void Awake()
     {
@@ -38,9 +44,44 @@ public class GameManager : MonoBehaviour
         IntroUI.SetActive(true);
     }
 
+    float CalculateScore()
+    {
+        return Time.time - PlayStartTime;
+    }
+
+    void SaveHighScore(){
+        int score = Mathf.FloorToInt(CalculateScore());
+        int currentHighScore = PlayerPrefs.GetInt("HighScore");
+        if(score > currentHighScore)
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+            PlayerPrefs.Save();
+        }
+    }
+
+    int GetHighScore(){
+        return PlayerPrefs.GetInt("HighScore");
+    }
+
+    public float CalculateGameSpeed()
+    {
+        if(State != GameState.Playing)
+        {
+            return 5f;
+        }
+        float speed = 5f + (0.5f * Mathf.FloorToInt(CalculateScore() / 10f));
+        return Mathf.Min(speed, 30f);
+    }
+
     // Update is called once per frame
     void Update()
     {
+        if(State == GameState.Playing)
+        {
+            ScoreText.text = "Score: " + Mathf.FloorToInt(CalculateScore());
+        } else if (State == GameState.Dead){
+            ScoreText.text = "High Score: " + GetHighScore();
+        }
         if(State == GameState.Intro && Input.GetKeyDown(KeyCode.Space))
         {
             State = GameState.Playing;
@@ -48,6 +89,7 @@ public class GameManager : MonoBehaviour
             EnemySpawner.SetActive(true);
             FoodSpawner.SetActive(true);
             GoldSpawner.SetActive(true);
+            PlayStartTime = Time.time;
         }
         if(State == GameState.Playing && Lives == 0)
         {
@@ -55,7 +97,9 @@ public class GameManager : MonoBehaviour
             EnemySpawner.SetActive(false);
             FoodSpawner.SetActive(false);
             GoldSpawner.SetActive(false);
+            DeadUI.SetActive(true);
             State = GameState.Dead;
+            SaveHighScore();
         }
         if(State == GameState.Dead && Input.GetKeyDown(KeyCode.Space))
         {
